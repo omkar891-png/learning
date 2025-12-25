28ba5fe88c41cfdef7453f51608c3fa8aaa3b447
omkar891 <omkarrajeshyadav723@gmail.com>
2025-12-25 21:16:00 +0530

    first commite

---
+++ python/01_Basic/Variable.py
@@ -0,0 +1,18 @@
+'''in python variable is a container that holds data value.
+ A variable is created the moment you first assign a value to it.
+ and it data type is  assign automatically based on the value you assign to it.
+ it mean you do not need to declare the variable type explicitly.
+ Variable names are case-sensitive, and they must start with a letter or an underscore (_), 
+ followed by letters, digits, or underscores.
+ Variable names cannot be the same as Python keywords (reserved words).
+ Here are some examples of variable assignments in Python:
+ '''
+age = 20
+first_Name = "omkar"
+Last_Name = 'Yadav'
+# class :-  {error is display here because class is a keyword in python}
+is_student = True
+pi = 3.14
+complex_number = 2 + 3j
+print(f" my name is {first_Name +" "+ Last_Name} and my age is {age}")
+print(f" my name is {first_Name +" "+ Last_Name}\n my age is {age}")
